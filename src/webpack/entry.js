5b716dff5cc858beb26747048a1e9a9ef2d11637
Sharon <sharon.peishan.kennedy@gmail.com>
2017-06-23 00:23:39 -0400

    added thank you page for form

--- src/webpack/entry.js
+++ src/webpack/entry.js
@@ -1,5 +1,7 @@
 import Instafeed from 'instafeed.js'
 import Masonry from 'masonry-layout'
+import smoothScroll from 'smooth-scroll'
+import Pikaday from 'pikaday'
 
 const feed = new Instafeed({
     get: 'user',
@@ -13,6 +15,73 @@ const feed = new Instafeed({
       img.type === 'image' &&
       (img.images.standard_resolution.height === img.images.standard_resolution.width)
     )
+})
+
+function postToGoogleSheets (data) {
+  const url = 'https://script.google.com/macros/s/AKfycbxb2hQxFXnKvKA67YG2YngTv5zLea5mPjsIecjslySsOG_X7_xp/exec';
+
+  $.ajax({
+    url: url,
+    data: data,
+    type: 'POST',
+    success: (res, status) => {
+      console.log(status);
+      const lang = localStorage.getItem('lang');
+      console.log(lang)
+      const url = (lang === 'es') ? `/${lang}/thanks` : '/thanks';
+      window.location.href = url;
+    },
+    error: (res) => {
+      console.log(res)
+    },
+  })
+}
+
+function sendContactFormData (e) {
+  e.preventDefault();
+  const dataArray = $(this).serializeArray();
+  let formData = {};
+  dataArray.forEach(function(field) {
+    formData[field.name] = field.value
+  });
+  postToGoogleSheets(formData);
+}
+
+function nextDay (date) {
+  const day = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+  return new Date(year, month, day + 1);
+}
+
+function initalizeDatePickers () {
+  const departureDate = nextDay(new Date())
+  const departureDateField = document.querySelector('input[name="departure-date"');
+  const returnDateField = document.querySelector('input[name="return-date"');
+  const returnDatePicker = new Pikaday({ field: returnDateField });
+  const departureDatePicker = new Pikaday({
+    field: departureDateField,
+    minDate: departureDate,
+    defaultDate: departureDate,
+    onSelect: (e) => {
+      const minReturnDate = nextDay(e);
+      returnDatePicker.setMinDate(minReturnDate);
+    }
+  });
+}
+
+document.getElementById('contact-form').addEventListener('submit', sendContactFormData);
+document.getElementById('en').addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('lang', 'en');
+  window.location.href = '/';
+});
+document.getElementById('es').addEventListener('click', (e) => {
+  e.preventDefault();
+  localStorage.setItem('lang', 'es');
+  window.location.href = '/es';
 });
 
+initalizeDatePickers();
 feed.run();
+smoothScroll.init();
